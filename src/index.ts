24608f962959a4fbad846f62824ce70bd75ca1cb
Dmitri Samo <unicatintern@gmail.com>
2021-11-18 21:00:19 +0100

    test3

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import appIcon from "../assets/iconSysMon.png";
 import { freemem } from "os";
 
 const win = new QMainWindow();
-win.setWindowTitle("DMS System Monitor Test2");
+win.setWindowTitle("DMS System Monitor Test3");
 win.setWindowOpacity(0.9);
 
 const os = require("os");
